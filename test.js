bc7da3305bf77af06a609a38bd93eddcaa231c06
Riku Kuisma <rikukuisma@icloud.com>
2023-11-29 23:05:13 +0200

    made a new ui

--- test.js
+++ test.js
@@ -1,3 +1,12 @@
-for (let i = 1; i <= 30; i++) {
-   console.log(i);
+let html = '';
+
+for (let i = 1; i <= 27; i++) {
+    html += `
+    <div class="card">
+        <img src="Yrittäjä Gaala Viimeiset Kuva Versiot kopio/kuva ${i}.jpg" alt="Image ${i}">
+        <a href="Yrittäjä Gaala Viimeiset Kuva Versiot kopio/kuva ${i}.jpg" download>Lataa Kuva</a>
+    </div>
+    `;
 }
+
+console.log(html);
